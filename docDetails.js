3bb6743b91fd1fdb9dc81a063f990e24d4d55e15
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-23 17:10:51 +0600

    Doc details  🚁

--- docDetails.js
+++ docDetails.js
@@ -4,7 +4,7 @@ const getparams = () => {
   fetch(`https://testing-8az5.onrender.com/doctor/list/${param}`)
     .then((res) => res.json())
     .then((data) => displayDetails(data));
-
+// Doc details 
   fetch(`https://testing-8az5.onrender.com/doctor/review/?doctor_id=${param}`)
     .then((res) => res.json())
     .then((data) => doctorReview(data));
